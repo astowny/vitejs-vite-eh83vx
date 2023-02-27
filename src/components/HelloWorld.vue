b70697e0cdee62d09a9b29abbec6eb391d5011eb
astowny <tony98@live.fr>
2023-02-27 13:04:47 +0100

    3 files modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
+const inputs = ref([
+  {
+    key: 0,
+    value: 0,
+  },
+]);
+inputs.value.push({
+  key: 1,
+  value: '5',
+});
+const sum = computed(() =>
+  inputs.value.map((x) => Number(x.value)).reduce((a, b) => a + b, 0)
+);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <input
+      v-for="inp in inputs"
+      :key="inp.key"
+      type="text"
+      v-model="inp.value"
+    />
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  Result sum : {{ sum }}
 </template>
 
 <style scoped>
